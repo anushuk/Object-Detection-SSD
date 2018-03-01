446237d21a038559f7a98450e6c500a5abd3de8a
Anubhav Shukla <anubhav237s@gmail.com>
2018-03-01 12:18:44 +0530

    Update object_detection.py

--- object_detection.py
+++ object_detection.py
@@ -35,7 +35,7 @@ net.load_state_dict(torch.load('ssd300_mAP_77.43_v2.pth', map_location = lambda 
 transform = BaseTransform(net.size, (104/256.0, 117/256.0, 123/256.0))
 
 # Doing some Object Detection on a video
-reader = imageio.get_reader('funny_dog.mp4')
+reader = imageio.get_reader('dog.mp4')
 fps = reader.get_meta_data()['fps']
 writer = imageio.get_writer('output.mp4', fps = fps)
 for i, frame in enumerate(reader):
